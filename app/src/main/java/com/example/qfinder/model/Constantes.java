496ab75e87a318104a6e8d26ffd03cec99ced7f0
juaness <sayayin4@gmail.com>
2025-04-08 11:16:15 -0500

    Conexión de base de datos a vista de registro usuario.

--- app/src/main/java/com/example/qfinder/model/Constantes.java
+++ app/src/main/java/com/example/qfinder/model/Constantes.java
@@ -1,4 +1,14 @@
 package com.example.qfinder.model;
 
 public class Constantes {
+    public static String NAME_BD ="qfinderZulema";
+    public static  int NUM_VERSION = 1;
+    public static String SENTENCIA_CREAR_USUARIO = "create table usuario(" +
+                "nombres VARCHAR(100) NOT NULL," +
+                "apellidos VARCHAR(100) NOT NULL," +
+                "email VARCHAR(100) NOT NULL," +
+                "telefono VARCHAR(10)," +
+                "nacimiento DATE," +
+                "password VARCHAR(20) NOT NULL" +
+            ");";
 }
