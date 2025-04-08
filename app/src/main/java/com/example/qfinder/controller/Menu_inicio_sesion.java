79c1169e41c99024bba7622ca1ba2196365190d1
MaicolSolano09 <maicolsolano09@gmail.com>
2025-04-08 17:42:33 -0500

    haciendo cambios

--- app/src/main/java/com/example/qfinder/controller/Menu_inicio_sesion.java
+++ app/src/main/java/com/example/qfinder/controller/Menu_inicio_sesion.java
@@ -17,10 +17,6 @@ public class Menu_inicio_sesion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_inicio_sesion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
